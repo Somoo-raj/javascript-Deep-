637c4b625d5f466001b97131212739b7f1db6c71
Sonu Kumar <skaul0618@gmail.com>
2026-09-21 16:34:28 +0530

    deleting properties & references

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
+// Array of objects reference trap
+
 const originalCart = [
     {id : 1, name: "Laptop", price: 32000 },
     {id : 2, name: "Phone", price: 12000 }
@@ -16,3 +18,16 @@ console.log(originalCart[0].price); // 34000
 console.log(updateCart.length); // 3
 console.log(updateCart[2].price); // 5000
 
+// deleting properties & references
+
+let player1 = {name: "virat", role: "Batsman"};
+let player2 = player1;
+
+delete player2.role; // delete string value and return undefined.
+
+player2 = {name: "Rohit", role: "Captain"}; // the pointer for player2 shifted to the new memory address.
+
+console.log(player1.role); // undefined
+console.log(player1.name); // virat
+console.log(player2); // {name: "Rohit", role: "Captain"}
+console.log(player1); // {name: "virat"}
